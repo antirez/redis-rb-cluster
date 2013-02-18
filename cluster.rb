8d8160393270bf5e6bd7c757d575165b4d17871b
antirez <antirez@gmail.com>
2013-02-18 17:38:34 +0100

    Initial skeleton of RedisCluster class. Nothing working (tm).

---
+++ cluster.rb
@@ -0,0 +1,59 @@
+require 'rubygems'
+require 'redis'
+require './crc16'
+
+class RedisCluster
+
+    RedisClusterHashSlots = 16384
+
+    def initialize(startup_nodes,connections)
+        @startup_nodes = startup_nodes
+        @max_connections = connections
+        @cur_connections = 0
+        @connections = {}
+        @slots = {}
+        initialize_slots
+    end
+
+    # Contact the startup nodes and try to fetch the hash slots -> instances
+    # map in order to initialize the @slots hash.
+    def initialize_slots
+        @startup_nodes.each{|n|
+            r = Redis.new(:host => n[:host], :port => n[:port])
+            r.cluster("nodes").each_line{|l|
+                fields = l.split(" ")
+                addr = fields[1]
+                slots = fields[-1]
+                addr = n[:host]+":"+n[:port].to_s if addr == ":0"
+                puts "#{addr}, #{slots}"
+            }
+        }
+    end
+
+    # Return the hash slot from the key.
+    def keyslot(key)
+        RedisClusterCRC16.crc16(key) % RedisClusterHashSlots
+    end
+
+    # Return the first key in the command, or nil if we don't know how to
+    # handle the specified command.
+    def get_key_from_command(argv)
+        case argv[0].to_s.downcase
+        when "set"
+            return argv[1]
+        when "get"
+            return argv[1]
+        else
+            return nil
+        end
+    end
+
+    # Dispatch commands.
+    def method_missing(*argv)
+        puts argv.join(",")
+    end
+end
+
+rc = RedisCluster.new([{:host => "127.0.0.1", :port => 6379}],2)
+rc.set("foo","bar")
+puts rc.get("foo")
