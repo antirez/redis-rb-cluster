94b29022153db925e485575f8e8bb62c8fbae500
antirez <antirez@gmail.com>
2013-11-11 09:17:33 +0100

    Example modified to restart from where it left.

--- example.rb
+++ example.rb
@@ -1,17 +1,30 @@
 require './cluster'
 
 startup_nodes = [
-    {:host => "127.0.0.1", :port => 6379},
-    {:host => "127.0.0.1", :port => 6380}
+    {:host => "127.0.0.1", :port => 7000},
+    {:host => "127.0.0.1", :port => 7001}
 ]
 rc = RedisCluster.new(startup_nodes,32,:timeout => 0.1)
-# rc.flush_slots_cache
-(0..1000000000).each{|x|
+
+last = false
+
+while not last
+    begin
+        last = rc.get("__last__")
+        last = 0 if !last
+    rescue => e
+        puts "error #{e.to_s}"
+        sleep 1
+    end
+end
+
+((last.to_i+1)..1000000000).each{|x|
     begin
         rc.set("foo#{x}",x)
         puts rc.get("foo#{x}")
-    rescue
-        puts "error"
+        rc.set("__last__",x)
+    rescue => e
+        puts "error #{e.to_s}"
     end
     sleep 0.1
 }
