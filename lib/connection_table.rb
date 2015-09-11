614e8da7d93b0b788b5f3f04aa804eb5fdca26a8
iandyh <feluccahuang@gmail.com>
2015-09-11 18:06:15 +0900

    lib for managing connections of nodes

---
+++ lib/connection_table.rb
@@ -0,0 +1,71 @@
+require 'connection_pool'
+require 'redis'
+
+class ConnectionTable
+
+  attr :table, :slots
+
+  def split_node(node)
+    host, port = node.split(':')
+    [host, port]
+  end
+
+  def initialize(max_connections)
+    @max_connections = max_connections
+    @table = {}
+    @slots = {}
+  end
+
+  def make_node_name(n)
+    name = "#{n[:host]}:#{n[:port]}"
+    n[:name] = name
+    name
+  end
+
+  def set_node_name!(n)
+    reset(make_node_name(n))
+  end
+
+  def new_pool(node)
+    host, port = split_node(node)
+    ConnectionPool.new(size: @max_connections) { Redis.new(:host => host, :port => port)}
+  end
+
+  def get_pool_by_node(node)
+    if !@table.has_key?(node)
+      pool = new_pool(node)
+      @table[node] = pool
+      return pool
+    end
+    @table[node]
+  end
+
+  def get_connection_by_node(node)
+    pool = get_pool_by_node(node)
+    pool.with do |conn|
+      return conn
+    end
+  end
+
+  def get_random_connection
+    random_node = @table.keys.sample
+    get_connection_by_node(random_node)
+  end
+
+  def get_connection_by_slot(slot)
+    node = @slots[slot]
+    get_connection_by_node(node)
+  end
+
+  def flush_slots_cache
+    @slots = {}
+  end
+
+  def reset(node)
+    @table[node] = new_pool(node)
+  end
+
+  def check_pid
+  end
+
+end
