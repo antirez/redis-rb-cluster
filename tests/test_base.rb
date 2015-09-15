1e0d57d040d032cabe9878e336dbdb9519ceef0b
iandyh <feluccahuang@gmail.com>
2015-09-15 14:34:38 +0900

    base test case. keep DRY

---
+++ tests/test_base.rb
@@ -0,0 +1,34 @@
+require './cluster'
+require 'test/unit'
+
+
+class TestBase < Test::Unit::TestCase
+
+  OK = "OK"
+  KEY = "asdf"
+
+  def setup
+    host = 'redis'
+    port1 = '7002'
+    port2 = '7003'
+
+    startup_nodes = [
+      {:host => host, :port => port1},
+      {:host => host, :port => port2},
+    ]
+    @rc = RedisCluster.new(startup_nodes, 2)
+  end
+
+  def teardown
+    @rc.flushdb
+  end
+
+  def get_keys_in_same_slot
+    ['bbb', 'mngopdrw']
+  end
+
+  def get_keys_in_diff_slot
+    ['bbb', 'aaa']
+  end
+
+end
