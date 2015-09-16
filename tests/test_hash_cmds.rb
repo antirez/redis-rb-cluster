636e9ea481d89c494c116dd2712bd9db23ebcb3f
iandyh <feluccahuang@gmail.com>
2015-09-16 11:19:38 +0900

    unit test for hash commands

---
+++ tests/test_hash_cmds.rb
@@ -0,0 +1,89 @@
+require './tests/test_base'
+
+class TestHashCmds < TestBase
+
+  FIELD1 = 'hello'
+  FIELD2 = 'world'
+
+  def test_hdel
+    @rc.hset(KEY, FIELD1, 'a')
+    assert_equal(1, @rc.hdel(KEY, FIELD1))
+  end
+
+  def test_hexists
+    @rc.hset(KEY, FIELD1, 'a')
+    assert_equal(false, @rc.hexists(KEY, FIELD2))
+    assert_equal(true, @rc.hexists(KEY, FIELD1))
+  end
+
+  def test_hget
+    @rc.hset(KEY, FIELD1, 'a')
+    assert_equal('a', @rc.hget(KEY, FIELD1))
+  end
+
+  def test_hgetall
+    @rc.hset(KEY, FIELD1, 'a')
+    @rc.hset(KEY, FIELD2, 'b')
+    t = {
+      FIELD1 => 'a',
+      FIELD2 => 'b'
+    }
+    assert_equal(t, @rc.hgetall(KEY))
+  end
+
+  def test_hincrby
+    @rc.hset(KEY, FIELD1, 1)
+    assert_equal(2, @rc.hincrby(KEY, FIELD1, 1))
+  end
+
+  def test_hincrbyfloat
+    @rc.hset(KEY, FIELD1, 1)
+    assert_equal(1.1, @rc.hincrbyfloat(KEY, FIELD1, 0.1))
+  end
+
+  def test_hkeys
+    @rc.hset(KEY, FIELD1, 'a')
+    @rc.hset(KEY, FIELD2, 'b')
+    assert_equal([FIELD1, FIELD2], @rc.hkeys(KEY).sort!)
+  end
+
+  def test_hlen
+    @rc.hset(KEY, FIELD1, 'a')
+    @rc.hset(KEY, FIELD2, 'b')
+    assert_equal(2, @rc.hlen(KEY))
+  end
+
+  def test_hmget
+    @rc.hset(KEY, FIELD1, 'a')
+    @rc.hset(KEY, FIELD2, 'b')
+    assert_equal(['a', 'b'], @rc.hmget(KEY, [FIELD1, FIELD2]).sort!)
+  end
+
+  def test_hmset
+    attrs = [FIELD1, 'a', FIELD2, 'b']
+    assert_equal(OK, @rc.hmset(KEY, *attrs))
+    assert_equal(['a', 'b'], @rc.hmget(KEY, [FIELD1, FIELD2]).sort!)
+  end
+
+  def test_hsetnx
+    @rc.hset(KEY, FIELD1, 'a')
+    assert_equal(false, @rc.hsetnx(KEY, FIELD1, 'a'))
+  end
+
+  # def test_hstrlen
+  #   @rc.hset(KEY, FIELD1, 'a')
+  #   assert_equal(1, @rc.hstrlen(KEY, FIELD1))
+  # end
+
+  def test_hvals
+    @rc.hset(KEY, FIELD1, 'a')
+    @rc.hset(KEY, FIELD2, 'b')
+    assert_equal(['a', 'b'], @rc.hvals(KEY).sort!)
+  end
+
+  def test_hscan
+    @rc.hset(KEY, FIELD1, 'a')
+    @rc.hset(KEY, FIELD2, 'b')
+    assert_equal(["0", [[FIELD1, "a"], [FIELD2, "b"]]], @rc.hscan(KEY, 0))
+  end
+end
